0f6afca20c1e4240edab2db63603bf0880225e03
Ian Groff <ian@iangroff.com>
2014-12-14 09:59:30 -0600

    ugh, gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,33 @@
-var concat = require('gulp-concat');
 var gulp   = require('gulp');
+var concat = require('gulp-concat');
+var coffee = require('gulp-coffee');
+var gutil  = require('gulp-util');
+var del    = require('del');
+var debug  = require('gulp-debug');
+var sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('scripts', function() {
-  gulp.src(['lib/ckeditor/ckeditor.js'])
-    .pipe(concat('all.js'))
+gulp.task('javascripts', function() {
+  return gulp.src(['lib/ckeditor/ckeditor.js'])
+    .pipe(concat('js.js'))
     .pipe(gulp.dest('./site/'))
 });
+
+gulp.task('coffee', function(){
+  return gulp.src(['src/*/*.coffee'])
+    .pipe(coffee())
+    .on('error', gutil.log)
+    .pipe(concat('coffee.js'))
+    .pipe(gulp.dest('./site/'))
+});
+
+gulp.task('all_scripts', ['coffee', 'javascripts'], function(cb){
+  gulp.src(['./site/coffee.js', './site/js.js'])
+    .pipe(concat('all.js'))
+    .pipe(gulp.dest('./site/'))
+    .on('end', function(){
+      del(['./site/coffee.js', './site/js.js'], cb);
+    })
+})
 
 gulp.task('style', function(){
   gulp.src(['lib/ckeditor/contents.css', 'src/css/style.css'])
@@ -13,4 +35,4 @@ gulp.task('style', function(){
     .pipe(gulp.dest('./site/'))
 });
 
-gulp.task('default', ['scripts', 'style']);
+gulp.task('default', ['javascripts', 'coffee', 'all_scripts', 'style']);
